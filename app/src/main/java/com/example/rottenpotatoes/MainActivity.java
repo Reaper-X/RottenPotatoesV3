42588afde4b145d5b2aa6578dd0b5728f2cfc8bd
Reaper-X <paulmichaeloliva@gmail.com>
2019-11-21 23:53:31 -0500

    update

--- app/src/main/java/com/example/rottenpotatoes/MainActivity.java
+++ app/src/main/java/com/example/rottenpotatoes/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAdapter = new MyAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
-
+//
         ClientFactory.init(this);
 
         FloatingActionButton btnAddMovie = findViewById(R.id.btn_addMovie);
